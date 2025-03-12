5d788392554c6ec100aaf80845d8f3ac68b00c8b
nmallebo <nmallebo@mo.ece.pdx.edu>
2025-03-11 23:49:20 -0700

    imem_design

--- rtl/imem.sv
+++ rtl/imem.sv
@@ -1,7 +1,19 @@
 // Read ONLY, no rd enable needed. Read will be combinational
 module imem(
-    input logic [31:0] addr,
-    output logic [31:0] data
+    input logic [31:0] addr,    // 32-bit input address
+    output logic [31:0] data    // 32-bit output instruction data 
 );
 
+    // Instruction memory array (32 locations, each 32-bit wide)
+    logic [31:0] i_arr [0:31];
+
+    // Initialize memory from an external file
+    initial begin
+        $readmemh("imem1_ini.mem", i_arr);
+    end
+
+    // Read instruction from memory (word-aligned address)
+    assign data = i_arr[addr[31:2]];
+
 endmodule
+
