a62a3d4903e6833d256c823071e127ab2fccccc3
nmallebo <nmallebo@mo.ece.pdx.edu>
2025-03-13 10:26:18 -0700

    p_fix1_update

--- rtl/imem.sv
+++ rtl/imem.sv
@@ -1,18 +1,14 @@
-// Read ONLY, no rd enable needed. Read will be combinational
+`timescale 1ns / 1ps
 module imem(
     input logic [31:0] addr,    // 32-bit input address
     output logic [31:0] data    // 32-bit output instruction data 
 );
-
-    // Instruction memory array (1024 locations, each 32-bit wide)
     logic [31:0] i_arr [0:1023];
 
-    // Initialize memory from an external file
     initial begin
         $readmemh("imem_ini.mem", i_arr);
     end
 
-    // Read instruction from memory (word-aligned address)
     assign data = i_arr[{addr[31:2],2'h0}];
 
 endmodule
--- rtl/s_type.sv
+++ rtl/s_type.sv
@@ -1,42 +1,29 @@
-/* module s_type(
-    input  logic [6:0]  opcode,
-    input  logic [11:0] imm,
-    input  logic [31:0] in1, in2, // Data of rs1 and rs2
-    output logic [31:0] d_addr, // address where data should be stored in dmem
-    output logic [31:0] d_data, // data going to dmem
-    output logic [3:0]  wr_en // Write enable signal to dmem, each bit enables 1 byte of dmem
-);
-
-endmodule */
-
 module s_type(
     input  logic [6:0]  opcode,
     input  logic [11:0] imm,
     input  logic [2:0] funct3,
-    input  logic [31:0] in1, in2, // Data of rs1 and rs2
-    output logic [31:0] d_addr, // Address where data should be stored in dmem
-    output logic [31:0] d_data, // Data going to dmem
-    output logic [3:0]  wr_en // Write enable signal to dmem, each bit enables 1 byte
+    input  logic [31:0] in1, in2, 
+    output logic [31:0] d_addr, 
+    output logic [31:0] d_data, 
+    output logic [3:0]  wr_en 
 );
 
-    // Compute store address (rs1 + sign-extended immediate)
     assign d_addr = in1 + {{20{imm[11]}}, imm}; 
 
-    // Default values
     always_comb begin
-        d_data = in2;  // Data to be stored in memory
-        wr_en = 4'b0000; // Default write enable signal
+        d_data = in2;  
+        wr_en = 4'b0000; 
 
-        case (opcode)
-            7'b0100011: begin // Store operations
-                case (funct3[2:0]) // Determining based on least significant bits of funct3
-                    2'b00: wr_en = 4'b0001; // SB (Store Byte)
-                    2'b01: wr_en = 4'b0011; // SH (Store Halfword)
-                    2'b10: wr_en = 4'b1111; // SW (Store Word)
-                    default: wr_en = 4'b0000; // Default case to prevent unintended writes
+        case (opcode && opcode ==  7'b0100011)
+            7'b0100011: begin 
+                case (funct3[2:0]) 
+                    2'b00: wr_en = 4'b0001; 
+                    2'b01: wr_en = 4'b0011; 
+                    2'b10: wr_en = 4'b1111; 
+                    default: wr_en = 4'b0000; 
                 endcase
             end
-            default: wr_en = 4'b0000; // No write enable for non-store instructions
+            default: wr_en = 4'b0000; 
         endcase
     end
 
