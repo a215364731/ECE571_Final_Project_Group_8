f3133fa64dfceaa8583475772b646d6f42740ac9
nmallebo <nmallebo@mo.ece.pdx.edu>
2025-03-13 09:38:33 -0700

    p_fix1

--- rtl/imem.sv
+++ rtl/imem.sv
@@ -4,16 +4,16 @@ module imem(
     output logic [31:0] data    // 32-bit output instruction data 
 );
 
-    // Instruction memory array (32 locations, each 32-bit wide)
-    logic [31:0] i_arr [0:31];
+    // Instruction memory array (1024 locations, each 32-bit wide)
+    logic [31:0] i_arr [0:1023];
 
     // Initialize memory from an external file
     initial begin
-        $readmemh("imem1_ini.mem", i_arr);
+        $readmemh("imem_ini.mem", i_arr);
     end
 
     // Read instruction from memory (word-aligned address)
-    assign data = i_arr[addr[31:2]];
+    assign data = i_arr[{addr[31:2],2'h0}];
 
 endmodule
 
