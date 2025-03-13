5953ac42c49dea247ae7716ef3036992733fbaba
nmallebo <nmallebo@mo.ece.pdx.edu>
2025-03-13 09:34:51 -0700

    p_fix1

--- rtl/i_type.sv
+++ rtl/i_type.sv
@@ -1,12 +1,3 @@
-/*module i_type(
-    input  logic [6:0]  opcode,
-    input  logic [2:0]  funct3,
-    input  logic [11:0] imm,
-    input  logic [31:0] in1, // Data of rs1
-    output logic [31:0] out // Data for rd
-);
-endmodule */
-
 `timescale 1ns / 1ps
 
 // Module for I-Type immediate instructions
@@ -19,7 +10,7 @@ module i_type(
 );
     
     always_comb begin
-        case(funct3)
+        case(funct3 && opcode ==  7'b0010011)
             3'b000: out = in1 + {{20{imm[11]}}, imm};  // ADDI (sign-extended immediate)
             3'b010: out = ($signed(in1) < $signed({{20{imm[11]}}, imm})) ? 32'b1 : 32'b0; // SLTI
             3'b011: out = (in1 < {{20'b0}, imm}) ? 32'b1 : 32'b0; // SLTIU (zero-extended immediate)
